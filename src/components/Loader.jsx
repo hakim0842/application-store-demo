c2746f21b07585055f55ec5012338e431332a2d4
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-10-08 17:14:30 +0600

    design a loader with full responsive

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import Logo from "./../assets/logo.png";
+export default function Loader() {
+  return (
+    <div className='md:text-9xl text-5xl font-extrabold animated-gradient h-full'>
+      <span className='flex flex-row justify-center items-center gap-1 md:gap-3'>
+        <span>L</span>
+        <img className='md:h-20 md:w-20 h-8 w-8 loader' src={Logo} alt='logo' />
+        <span>ading</span>
+      </span>
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
+import Loader from "../components/Loader";
 import Overview from "../components/Overview";
 import Hero from "./../components/Hero";
 export default function HomePage() {
   return (
     <div>
-      <section className='p-10 lg:p-20'>
+      {/* <section className='p-10 lg:p-20'>
         <Hero />
       </section>
       <section className='-mt-10 lg:-mt-20'>
         <Overview />
-      </section>
+      </section> */}
+      <Loader />
     </div>
   );
 }

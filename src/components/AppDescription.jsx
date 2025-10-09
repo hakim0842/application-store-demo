64c409aa06a84ba9dbc9b5efc333a2af282e4460
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-10-09 22:26:04 +0600

    when click install button install app and text change to uninstall

---
+++ src/components/AppDescription.jsx
@@ -0,0 +1,3 @@
+export default function AppDescription() {
+  return <div></div>;
+}
--- src/components/AppDetailsHero.jsx
+++ src/components/AppDetailsHero.jsx
@@ -1,10 +1,14 @@
 import { ToastContainer, toast } from "react-toastify";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FiDownload } from "react-icons/fi";
 import { TiStar } from "react-icons/ti";
 import { BiSolidLike } from "react-icons/bi";
 import AppDetailsHeroOverviewImage from "./AppDetailsHeroOverviewImage";
-import { installApp } from "./../utils/localStorage.js";
+import {
+  getInstalledApps,
+  installApp,
+  uninstallApp,
+} from "./../utils/localStorage.js";
 export default function AppDetailsHero({
   id,
   image,
@@ -12,6 +16,8 @@ export default function AppDetailsHero({
   companyName,
   size,
 }) {
+  const [data, setData] = useState(getInstalledApps());
+
   useEffect(() => {
     if (title) {
       document.title = `${title} | App Details`;
@@ -21,16 +27,26 @@ export default function AppDetailsHero({
     return () => (document.title = "App Store Demo");
   }, [title]);
 
-  function onInstall(id) {
-    const checked = installApp(id);
-    toast(`${checked ? "Already" : ""} Installed ${title}!`);
+  function handelClick(id, mode) {
+    if (mode === "install") {
+      const checked = installApp(id);
+      toast(`${checked ? "Already" : ""} Installed ${title}!`);
+      setData(getInstalledApps());
+    }
+    if (mode === "uninstall") {
+      uninstallApp(id);
+      toast(` Uninstalled ${title}!`);
+      setData(getInstalledApps());
+    }
   }
+
+  const isInstalled = data.includes(id);
 
   return (
     <>
       <div className='flex flex-col sm:flex-row gap-4 md:gap-7 lg:gap-10 mt-20'>
         <img
-          className='lg:w-80 lg:h-80 md:w-60  sm:w-1/2 sm:h-64 h-44 w-1/2'
+          className='lg:w-80 lg:h-80 md:w-60  sm:w-1/2 sm:h-64 h-44 w-1/2 shadow-md'
           src={image}
           alt={title}
         />
@@ -67,10 +83,12 @@ export default function AppDetailsHero({
             />
           </div>
           <button
-            onClick={() => onInstall(id)}
+            onClick={() =>
+              handelClick(id, `${isInstalled ? "uninstall" : "install"}`)
+            }
             className='btn text-sm lg:text-xl w-full md:w-1/2 xl:w-1/3 btn-ghost btn-primary text-white hover:text-black bg-green-400 border-0 btn-md lg:btn-lg'
           >
-            Install Now {`(${size} MB)`}
+            {isInstalled ? "Uninstall" : `Install Now (${size} MB)}`}
           </button>
         </div>
       </div>

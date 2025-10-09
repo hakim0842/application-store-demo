a6bd7b22a53c6e62aad49d0d9ffeefe6576005d6
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-10-09 23:17:58 +0600

    bug fix - 3

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -23,7 +23,7 @@ export default function MobileNav() {
         </div>
         <ul
           tabIndex={0}
-          className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow bg-gray-50'
+          className='menu menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-2 shadow bg-gray-50'
         >
           <li>
             <Link to='/'>Home</Link>
